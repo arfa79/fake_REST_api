56950a908dfa90c5c51d87f883b3a1f8c1f86b0a
ARFA79 <arfa79lg@gmail.com>
2023-08-27 21:25:48 +0330

    auth app

---
+++ auth_app/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')  # Redirect to app's main page
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
